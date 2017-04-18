9590b052f3eb90fc63445e2d244dd4cecf9243eb
andersononus@hotmail.com <andersononus@hotmail.com>
2017-04-18 20:19:37 -0300

    add files

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,6 +1,22 @@
 module.exports = function(app){
-  app.get("/produtos", function(req,res){
-    res.render('produtos/lista');
+app.get("/produtos",function(req,res){
+  var mysql = require('mysql');
+
+  var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'caelum',
+    database:'casadocodigo'
   });
 
+  connection.query('select * from livros', 
+  
+  function(err,result,fields){
+   res.render('produtos/lista',{lista:result});
+ // res.send(result);
+}
+);
+connection.end();
+   
+});
 };
